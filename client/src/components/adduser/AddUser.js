b39a292e0d7afdfa2ecb4897d978e54f10316cb7
manikanta1601 <pmanikantareddy16@gmail.com>
2023-06-30 16:28:19 +0530

    aws deploy

--- client/src/components/adduser/AddUser.js
+++ client/src/components/adduser/AddUser.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState,useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import './AddUser.css';
--- client/src/components/rootlayout/RootLayout.js
+++ client/src/components/rootlayout/RootLayout.js
@@ -1,38 +1,54 @@
-import React from 'react'
-import NavbarMain from "../navbar/NavbarMain"
-import { domainContext } from "../../context/DomainContextProvider";
-import Footer from "../footer/Footer"
-import { Outlet } from 'react-router-dom'
-import "./rootlayout.css"
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import NavbarMain from '../navbar/NavbarMain';
+import { domainContext } from '../../context/DomainContextProvider';
+import Footer from '../footer/Footer';
+import { Outlet } from 'react-router-dom';
+import './rootlayout.css';
+
 function RootLayout() {
-  let [domain,setDomain]=useContext(domainContext)
-  useEffect(()=>{
-    let url=window.location.href;
-    let baseURL = url.split("/").slice(0, 3).join("/")
-    setDomain(baseURL.replace("://","://server."))
-    console.log(domain)
-  },[location])
-        
+  const location = useLocation();
+  const [path, setPath] = useState('none');
+  const [domain, setDomain] = useContext(domainContext);
+
+  useEffect(() => {
+    const url = location.pathname.replace('/', '');
+    if (url.length === 0) {
+      setPath('home');
+    } else {
+      setPath(url);
+    }
+  }, [location]);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const baseURL = url.split('/').slice(0, 3).join('/');
+    setDomain(baseURL.replace('://', '://server.'));
+    console.log(domain);
+  }, [location]);
+
   return (
-    <div style={{
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      backgroundImage: "url(https://img.freepik.com/free-vector/gradient-background-vector-spring-green_53876-117272.jpg?size=626&ext=jpg&ga=GA1.2.539893135.1686191687&semt=ais)",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-    }}>
-     <NavbarMain/>
-     {/* placeholder */}
-     <div >
-     <Outlet/>
-     </div>
-     <div style={{marginTop:"auto"}}>
-     <Footer/>
-     </div>
-     
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundImage:
+          'url(https://img.freepik.com/free-vector/gradient-background-vector-spring-green_53876-117272.jpg?size=626&ext=jpg&ga=GA1.2.539893135.1686191687&semt=ais)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <NavbarMain />
+      <div>
+        {/* placeholder */}
+        <Outlet />
+      </div>
+      <div style={{ marginTop: 'auto' }}>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default RootLayout
+export default RootLayout;
